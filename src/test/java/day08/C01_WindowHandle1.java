c3d8c56f9ae233a01138749c049649fbb686e498
AlperenYcdl <yucedalalper@gmail.com>
2023-01-05 20:39:07 +0100

    rr3

--- src/test/java/day08/C01_WindowHandle1.java
+++ src/test/java/day08/C01_WindowHandle1.java
@@ -8,19 +8,24 @@ import java.util.Set;
 public class C01_WindowHandle1 extends TestBase{
 
     //    Tests package’inda yeni bir class olusturun: WindowHandle
-//    https://the-internet.herokuapp.com/windows adresine gidin.
+
+    //1- https://the-internet.herokuapp.com/windows adresine gidin.
     @Test
     public void handleWindowTest() throws InterruptedException {
         driver.get("https://the-internet.herokuapp.com/windows");
-//        ILK SAYFANIN IDSINI ALALIM
+
+    //2-      ILK SAYFANIN IDSINI ALALIM
         String window1Handle = driver.getWindowHandle();
-//    Sayfadaki textin “Opening a new window” olduğunu doğrulayın.
-        String window1Text = driver.findElement(By.xpath("//h3")).getText();
+
+    //3-  Sayfadaki textin “Opening a new window” olduğunu doğrulayın.
+        String window1Text = driver.findElement(By.xpath("//h3")).getText();   //bir tane h3 oldugunu icin bunu alabiliriz. .tagNae de "h3" kullanilir
         Assert.assertEquals("Opening a new window",window1Text);
-//    Sayfa başlığının(title) “The Internet” olduğunu doğrulayın.
+
+    //4-  Sayfa başlığının(title) “The Internet” olduğunu doğrulayın.
         String window1Title = driver.getTitle();
         Assert.assertEquals("The Internet",window1Title);
-//    Click Here butonuna basın.
+
+    //5-  Click Here butonuna basın.
         driver.findElement(By.linkText("Click Here")).click();
         Thread.sleep(3000);
 
@@ -39,17 +44,22 @@ public class C01_WindowHandle1 extends TestBase{
 //     BU NOKTADA DRIVER 2. PENCEREDE
 //    Acilan yeni pencerenin sayfa başlığının (title) “New Window” oldugunu dogrulayin.
         Assert.assertEquals("New Window",driver.getTitle());
+
 //    Sayfadaki textin “New Window” olduğunu doğrulayın.
         Assert.assertEquals("New Window",driver.findElement(By.xpath("//h3")).getText());
         String window2Handle = driver.getWindowHandle();
+
 //    Bir önceki pencereye geri döndükten sonra sayfa başlığının “The Internet” olduğunu  doğrulayın.
         Thread.sleep(3000);
+
 //        1. windowa gecis yapiyorum
         driver.switchTo().window(window1Handle);
         Assert.assertEquals("The Internet",driver.getTitle());
         Thread.sleep(3000);
+
 //        2. windowa tekrar gecelim
         driver.switchTo().window(window2Handle);
+
         Thread.sleep(3000);
 //        1. windowa tekrar gecelim
         driver.switchTo().window(window1Handle);
